5203acfcfd2dc6df13eca33fc53646814f4401c0
malko <jgotti@jgotti.com>
2026-02-16 23:54:08 +0100

    🐛 Fix vm creation

--- host/create-vm.sh
+++ host/create-vm.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
+CONFIG_FILE="$HOME/.config/aibox/aibox.conf"
 source "$(dirname "$0")/../shared-funcs.sh"
+source "$(dirname "$0")/../config-funcs.sh"
+init_config_file
+
+LIBVIRT_DEFAULT_URI=$(get_config "LIBVIRT_DEFAULT_URI" "qemu:///system")
 
 check_command virsh
 check_command virt-install
@@ -26,13 +31,21 @@ print_info "=== VM Configuration ==="
 ISO_PATH=$(prompt "Path to Ubuntu ISO" "")
 VCPU=$(prompt "Number of CPUs" "4")
 RAM=$(prompt "RAM in MB" "4096")
-DISK_SIZE=$(prompt "Disk size (e.g., 20G)" "20G")
+DISK_SIZE=$(prompt "Disk size in GB (e.g., 20)" "20")
 DISK_PATH=$(prompt "vm disk file path" "/var/lib/libvirt/images/${VM_NAME}.qcow2")
 NETWORK=$(prompt "Network" "default")
 
 print_info "Creating VM '$VM_NAME'..."
 
+# TODO check for default network or try to create if from virsh net-define /usr/share/libvirt/networks/default.xml
+virsh net-info default >/dev/null 2>&1 || echo "Erreur: Le réseau 'default' n'existe pas."
+if [ "$(virsh -c "$LIBVIRT_DEFAULT_URI" net-info default | grep 'Active' | awk '{print $2}')" = "no" ]; then
+    virsh -c "$LIBVIRT_DEFAULT_URI" net-start default
+    echo "Réseau 'default' démarré."
+fi
+
 virt-install \
+    --connect "$LIBVIRT_DEFAULT_URI" \
     --name "$VM_NAME" \
     --vcpus "$VCPU" \
     --ram "$RAM" \
@@ -41,7 +54,7 @@ virt-install \
     --network network="$NETWORK" \
     --graphics none \
     --console pty,target_type=serial \
-    --location "$ISO_PATH" \
+    --location "$ISO_PATH",kernel=casper/vmlinuz,initrd=casper/initrd \
     --extra-args 'console=ttyS0,115200n8 serial'
 
 print_success "VM created!"
